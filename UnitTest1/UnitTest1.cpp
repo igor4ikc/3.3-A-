ce3885b2371eeb41e9e2d8303d8d1169107729e7
igor4ikc <igor4ikc@users.noreply.github.com>
2021-04-13 21:51:44 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../3.3(A)/Vector.cpp"
+#include "../3.3(A)/Vector.h"
+#include "../3.3(A)/Vector2D.h"
+#include "../3.3(A)/Vector2D.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Vector A(3, 4);
+			Assert::AreEqual(A.Modul(), 5.0);
+		}
+	};
+}
